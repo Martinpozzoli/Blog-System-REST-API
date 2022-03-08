ac090f99c6c24616b71f504b9be5b25218261ea6
Martin Pozzoli <martinpozzoli2@gmail.com>
2022-03-08 19:29:24 -0300

    Swagger Documentation added

--- src/main/java/com/blog/system/config/SecurityConfig.java
+++ src/main/java/com/blog/system/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,7 +23,7 @@ import com.blog.system.security.JwtAuthenticationFilter;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
@@ -51,8 +52,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
 			.antMatchers("/api/auth/**").permitAll()
-			.anyRequest()
+			.antMatchers("/**", "/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll() //Para el funcionamiento de swagger
+			.anyRequest()		//Para visualizar la documentacion generada: http://localhost:8080/swagger-ui.html o http://localhost:8080/v2/api-docs
 			.authenticated();
+		
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
@@ -66,4 +69,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
+	
+	//Swagger Resources
+    
+    //Si se usa Spring Security se agrega la siguiente Configuracion
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+          .ignoring()
+            .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**")
+            .antMatchers(HttpMethod.OPTIONS,"/**");
+    }
+
 }
